e740ea3eb216a0831e9be385b4c0f5b0fd11277a
Waakul <waakul72@gmail.com>
2024-03-22 16:18:11 +0300

    thread

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from threading import Thread
 import server
 
 app = Flask(__name__)
 
 @app.route("/")
 def serverrun():
-    server.client.run(data_from_websocket=True, thread=True)
+    Thread(server.client.run).start()
     return "The server is up!"
