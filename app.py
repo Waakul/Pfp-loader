4964ad853516675174cabc3ad837cc754b5d9cd1
Waakul <waakul72@gmail.com>
2024-03-22 16:31:56 +0300

    fix

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def serverrun():
-    Thread(server.client.run).start()
+    server.client.run(data_from_websocket=True, thread=True)
     return "The server is up!"
